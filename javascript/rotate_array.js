170207c69ca2b5828925db81f1d36b9cd5348924
shu Lin <bbqlinshu@gmail.com>
2022-01-02 17:22:49 -0500

    Completed assignment

--- javascript/rotate_array.js
+++ javascript/rotate_array.js
@@ -1,5 +1,19 @@
 function rotateArray(arr, k) {
   // type your code here
+  if(arr.length === 0){
+    return []
+  }else{
+    const r = k%(arr.length)
+    if(r===0){
+      return arr
+    }else{
+      for (let i=arr.length-1; i>=arr.length-k; i--){
+        const temp = arr.pop()
+        arr.unshift(temp)
+      }
+    }
+  }
+  return arr
 }
 
 if (require.main === module) {
@@ -16,6 +30,11 @@ if (require.main === module) {
 
   console.log("Expecting: [1, 2, 3]");
   console.log("=>", rotateArray([1, 2, 3], 3));
+
+  console.log("");
+
+  console.log("Expecting: [4, 5, 1, 2, 3]");
+  console.log("=>", rotateArray([1, 2, 3, 4, 5], 12));
 }
 
 module.exports = rotateArray;
